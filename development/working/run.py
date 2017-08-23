8b18faf5ebcd44fd0547a8c4c5df96421165fd83
Philipp Eisenhauer <eisenhauer@policy-lab.org>
2017-08-23 08:47:28 +0200

    edits#

--- development/working/run.py
+++ development/working/run.py
@@ -1,15 +1,15 @@
 """ This module allows to manually explore the capabilities of the grmpy package.
 """
 import sys
+
+sys.path.insert(0, "../../")
+
 from grmpy.simulation.simulation import simulate
 from grmpy.read.read import read
 from grmpy.test.random_init import generate_random_dict
 from grmpy.test.random_init import print_dict
 
-sys.path.insert(0, "../../")
 
 dict_ = generate_random_dict()
 print_dict(dict_)
-dict_ = read('test.grmpy.ini')
-print(dict_)
-simulate(dict_)
+simulate('test.grmpy.ini')
