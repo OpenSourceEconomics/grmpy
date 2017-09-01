8d27399b56e9c8a5548a728cb6d60c0231bb9ffa
Sebastian Becker <s3sebeck@uni-bonn.de>
2017-09-01 09:25:47 +0200

    Update conftest.py

--- grmpy/test/conftest.py
+++ grmpy/test/conftest.py
@@ -1,4 +1,4 @@
-""" This module provides the fixtures for the PYTEST runs."""
+"""This module provides the fixtures for the PYTEST runs."""
 import tempfile
 import os
 
@@ -8,11 +8,11 @@ import pytest
 
 @pytest.fixture()
 def set_seed():
-    """ Each test is executed with the same random seed."""
+    """Each test is executed with the same random seed."""
     np.random.seed(1223)
 
 
 @pytest.fixture()
 def fresh_directory():
-    """ Each test is executed in a fresh directory."""
+    """Each test is executed in a fresh directory."""
     os.chdir(tempfile.mkdtemp())
