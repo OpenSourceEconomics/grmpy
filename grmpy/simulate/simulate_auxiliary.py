ffdadcb0bbe7d5abf64e4b30e12f123d3916bd36
Philipp Eisenhauer <eisenhauer@policy-lab.org>
2017-09-07 19:52:05 +0200

    new layout information file

--- grmpy/simulate/simulate_auxiliary.py
+++ grmpy/simulate/simulate_auxiliary.py
@@ -1,6 +1,6 @@
-""" This module provides auxiliary functions for the simulate.py module. It includes simulation pro-
-cesses of the unobservable and endogeneous variables of the model as well as functions regarding the
-info file output.
+""" This module provides auxiliary functions for the simulate.py module. It includes simulation
+processes of the unobservable and endogeneous variables of the model as well as functions regarding
+the info file output.
 """
 import pandas as pd
 import numpy as np
@@ -93,209 +93,62 @@ def write_output(end, exog, err, source):
     return df
 
 
-def collect_information(data_frame):
-    """The function collects the required information for the info file."""
-    # Number of individuals:
-    indiv = data_frame.shape[0]
-
-    # Counts by treatment status
-    treated_num = data_frame[data_frame.D == 1].shape[0]
-    untreated_num = indiv - treated_num
-
-    no_treatment, all_treatment = _adjust_collecting(treated_num, untreated_num)
-    # Average Treatment Effect
-
-    ate, tt, tut, mean_over, sd_over, quant_over, mean_treat, sd_treat, quant_treat, mean_untreat, \
-    sd_untreat, quant_untreat = _calc_parameters(data_frame, no_treatment, all_treatment)
-
-    data = {
-        'Number of Agents': indiv, 'Treated Agents': treated_num, 'Untreated Agents': untreated_num,
-        'Mean Untreated': mean_untreat, 'Mean Treated': mean_treat, 'Mean All': mean_over,
-        'Quantiles Untreated': quant_untreat, 'Quantiles Treated': quant_treat, 'Quantiles All':
-        quant_over, 'Std Untreated': sd_untreat, 'Std Treated': sd_treat, 'Std All': sd_over,
-        'ate': ate, 'tut': tut, 'tt': tt,
-    }
-
-    return data
-
-
 def print_info(data_frame, coeffs, file_name):
     """The function writes an info file for the specific data frame."""
-    data_ = collect_information(data_frame)
-
-    labels = []
-    labels += ['Simulation', 'Distribution of Outcomes', 'Distribution of Effects']
-    labels += ['Model Parameterization']
 
     with open(file_name + '.grmpy.info', 'w') as file_:
 
-        for label in labels:
-            file_.write('\n' + label + '\n\n')
+        # First we note some basic information ab out the dataset.
+        header = '\n\n Number of Observations \n\n'
+        file_.write(header)
 
-            if label == 'Simulation':
-                structure = ['Number of Agents', 'Treated Agents', 'Untreated Agents']
+        info = [data_frame.shape[0], (data_frame['D'] == 1).sum(), (data_frame['D'] == 0).sum()]
 
-                for s in structure:
-                    str_ = '{0:<20} {1:20}\n'
+        fmt = '  {:<10}' + ' {:>20}' * 1 + '\n\n'
+        file_.write(fmt.format(*['', 'Count']))
 
-                    file_.write(str_.format(s + ':', data_[s]))
+        for i, label in enumerate(['All', 'Treated', 'Untreated']):
+            str_ = '  {:<10} {:20}\n'
+            file_.write(str_.format(*[label, info[i]]))
 
-            elif label == 'Distribution of Outcomes':
-                _print_dist(data_, file_)
+        # Second, we describe the distribution of outcomes and effects.
+        for label in ['Outcomes', 'Effects']:
 
-            elif label == 'Distribution of Effects':
-                _print_effects(data_, file_)
+            header = '\n\n Distribution of ' + label + '\n\n'
+            file_.write(header)
 
-            else:
-                str_ = '{0:>10} {1:>18}\n\n'.format('Identifier', 'Value')
-                file_.write(str_)
+            fmt = '  {:<10}' + ' {:>20}' * 5 + '\n\n'
+            args = ['', 'Mean', 'Std-Dev.', '25%', '50%', '75%']
+            file_.write(fmt.format(*args))
 
-                value = np.append(np.append(coeffs[0], coeffs[1]), np.append(coeffs[2], coeffs[3]))
-                len_ = len(value) - 1
-                for i in range(len_):
-                    file_.write('{0:>10} {1:>20.4f}\n'.format(str(i), value[i]))
+            for group in ['All', 'Treated', 'Untreated']:
 
-
-def _print_effects(data_, file_name):
-    """The function writes the effect information to the init file."""
-    structure = ['ate', 'tt', 'tut']
-    sub_structure = ['', '_0.1', '_0.25', '_0.5', '_0.75', '_0.9']
-    str_ = '{0:>10} {1:>18} {2:>18} {3:>18} {4:>18} {5:>18} {6:>18}\n\n'.format(
-        'Effect', 'Overall', '0.1', '0.25', '0.5', '0.75', '0.9')
-    file_name.write(str_)
-
-    for label in structure:
-        str_ = '{0:>10}'.format(label.upper())
-        for s in sub_structure:
-            entry = label + s
-            if isinstance(data_[label], str):
-                str_ += '{0:>19}'.format(data_[label])
-            else:
-                str_ += '{0:>19.4f}'.format(data_[label][entry])
-        file_name.write(str_ + '\n')
-
-
-def _print_dist(data_, file_name):
-    """The function writes the distributional information to the init file."""
-    fmt = '  {:<10}' + ' {:>20}' * 5 + '\n\n'
-    args = ['Group', 'Mean', 'Std-Dev.', '2.Decile', '5.Decile', '8.Decile']
-    file_name.write( fmt.format(*args))
-
-    labels = ['All', 'Treated', 'Untreated']
-    for label in labels:
-        str_ = '  {0:<10}'.format(label)
-
-        structure = ['Mean ', 'Std ', 'Quantiles ']
-
-        for s in structure:
-            s += label
-            if s.startswith('Quantiles'):
-                if isinstance(data_[s], str):
-                    q02 = data_[s]
-                    q05 = data_[s]
-                    q08 = data_[s]
-                    str_ += '{0:>20} {1:>20} {2:>20}'.format(q02, q05, q08)
+                if label == 'Outcomes':
+                    object = data_frame['Y']
+                elif label == 'Effects':
+                    object = data_frame['Y1'] - data_frame['Y0']
                 else:
-                    q02 = data_[s].loc[0.2]
-                    q05 = data_[s].loc[0.5]
-                    q08 = data_[s].loc[0.8]
-                    str_ += ' {0:>20.4f} {1:>20.4f} {2:>20.4f}'.format(q02, q05, q08)
-            else:
-                if isinstance(data_[s], float):
-                    str_ += '{0:>21.4f}'.format(data_[s])
+                    raise AssertionError
+
+                if group == 'Treated':
+                    object = object[data_frame['D'] == 1]
+                elif group == 'Untreated':
+                    object = object[data_frame['D'] == 0]
                 else:
-                    str_ += '{0:>20}'.format(data_[s])
+                    pass
 
-        file_name.write(str_ + '\n')
+                fmt = '  {:<10}' + ' {:>20.4f}' * 5 + '\n'
+                info = list(object.describe().tolist()[i] for i in [1, 2, 4, 5, 6])
+                file_.write(fmt.format(* [group] + info))
 
+        # Third we write out the parametrization of the model.
+        header = '\n\n Parametrization \n\n'
+        file_.write(header)
+        str_ = '  {0:>10} {1:>20}\n\n'.format('Identifier', 'Value')
+        file_.write(str_)
 
-def _calc_parameters(data_frame, no_treatment, all_treatment):
-    """The function calculates the distributional information and effects for the info file. The
-    calculation depends on the specific case that occurs (no treated agents, no untreated agents or
-    treated and untreated agents).
-    """
-    out_quant = data_frame.Y.quantile([0.1, 0.25, 0.5, 0.75, 0.9])
+        value = np.append(np.append(coeffs[0], coeffs[1]), np.append(coeffs[2], coeffs[3]))
+        len_ = len(value) - 1
+        for i in range(len_):
+            file_.write('  {0:>10} {1:>20.4f}\n'.format(str(i), value[i]))
 
-    # Average Treatment effect
-    ate_ = np.mean(data_frame.Y1 - data_frame.Y0)
-    ate = {'ate': ate_}
-    for i in [0.1, 0.25, 0.5, 0.75, 0.9]:
-        ate['ate_' + str(i)] = calculate_ate(data_frame, out_quant, i)
-
-    # Treatment on the Treated
-    if no_treatment:
-        tt = '---'
-        mean_treat = "---"
-        sd_treat = "---"
-        quant_treat = "---"
-    else:
-        tt_ = np.mean(data_frame.Y1[data_frame.D == 1]) - np.mean(data_frame.Y0[data_frame.D == 1])
-        tt = {'tt': tt_}
-        for i in [0.1, 0.25, 0.5, 0.75, 0.9]:
-            tt['tt_' + str(i)] = calculate_tt(data_frame, out_quant, i)
-        mean_treat = np.mean(data_frame.Y[data_frame.D == 1])
-        sd_treat = np.std(data_frame.Y[data_frame.D == 1])
-        quant_treat = data_frame.Y[data_frame.D == 1].quantile([0.2, 0.5, 0.8])
-
-    # Treatment on Untreated
-    if all_treatment:
-        tut = "---"
-        mean_untreat = "---"
-        quant_untreat = "---"
-        sd_untreat = "---"
-    else:
-        tut_ = np.mean(data_frame.Y1[data_frame.D == 0]) - np.mean(data_frame.Y0[data_frame.D == 0])
-        tut = {'tut': tut_}
-        for i in [0.1, 0.25, 0.5, 0.75, 0.9]:
-            tut['tut_' + str(i)] = calculate_tt(data_frame, out_quant, i)
-        mean_untreat = np.mean(data_frame.Y[data_frame.D == 0])
-        quant_untreat = data_frame.Y[data_frame.D == 0].quantile([
-            0.2, 0.5, 0.8])
-        sd_untreat = np.std(data_frame.Y[data_frame.D == 0])
-
-    # Average observed wage overall and by treatment status
-    mean_over = np.mean(data_frame.Y)
-    sd_over = np.std(data_frame.Y)
-    quant_over = data_frame.Y.quantile([0.2, 0.5, 0.8])
-
-    return ate, tt, tut, mean_over, sd_over, quant_over, mean_treat, sd_treat, quant_treat, \
-           mean_untreat, sd_untreat, quant_untreat
-
-
-def _adjust_collecting(treated_num, untreated_num):
-    """The function determines if there are only treated, only untreated or treated and untreated
-    individuals.
-    """
-    if treated_num == 0:
-        no_treatment = True
-    else:
-        no_treatment = False
-
-    if untreated_num == 0:
-        all_treatment = True
-    else:
-        all_treatment = False
-
-    assert no_treatment != all_treatment or no_treatment == all_treatment is False
-
-    return no_treatment, all_treatment
-
-
-def calculate_ate(df, quant, q):
-    """The function calculates the average treatment effect for a given quantile."""
-    ate = np.mean(df.Y1[df.Y <= quant.loc[q]] - df.Y0[df.Y <= quant.loc[q]])
-    return ate
-
-
-def calculate_tt(df, quant, q):
-    """The function calculates the treatment on treated effect for a given quantile."""
-    tt = np.mean(df.Y1[(df.D == 1) & (df.Y <= quant.loc[q])]) \
-         - np.mean(df.Y0[(df.D == 1) & (df.Y <= quant.loc[q])])
-    return tt
-
-
-def calculate_tut(df, quant, q):
-    """The function calculates the treatment on untreated effect for a given quantile."""
-    tut = np.mean(df.Y1[(df.D == 0) & (df.Y <= quant.loc[q])]) \
-          - np.mean(df.Y0[(df.D == 0) & (df.Y <= quant.loc[q])])
-    return tut
