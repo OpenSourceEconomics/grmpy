1cdcc8bd01d65e53c92efdd1781deb9ecffefc99
Philipp Eisenhauer <eisenhauer@policy-lab.org>
2017-09-07 19:58:02 +0200

    nuisance edit

--- development/working/run.py
+++ development/working/run.py
@@ -7,6 +7,6 @@ from grmpy.test.random_init import generate_random_dict
 from grmpy.simulate.simulate import simulate
 from grmpy.test.random_init import print_dict
 
-#dict_ = generate_random_dict()
-#print_dict(dict_)
+dict_ = generate_random_dict()
+print_dict(dict_)
 simulate('test.grmpy.ini')
