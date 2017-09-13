7324158357b47942e403ae9c1aa3d7e5c541b6a5
Philipp Eisenhauer <eisenhauer@policy-lab.org>
2017-09-13 13:32:32 +0200

    starting on cleanup of regression test runner

--- development/tests/regression/run.py
+++ development/tests/regression/run.py
@@ -3,8 +3,7 @@ generates a random init file for each seed, simulates the resulting dataframe, s
 the sum and the generated dictionary in /grmpy/test/resources/ as a json file. In the second step it
 reads the json file, and loops over all entries. For each element it prints an init file, simulates
 the dataframe, sums it up again and compares the sum from the first step with the one from the
-second step.
- """
+second step."""
 
 import glob
 import json
@@ -12,9 +11,12 @@ import os
 
 import numpy as np
 
+import grmpy
 from grmpy.test.random_init import generate_random_dict
 from grmpy.test.random_init import print_dict
 from grmpy.simulate.simulate import simulate
+
+PACKAGE_DIR = os.path.dirname(grmpy.__file__)
 
 NUM_TESTS = 100
 
@@ -23,7 +25,7 @@ seeds = np.random.randint(0, 1000, size=NUM_TESTS)
 dir = os.path.dirname(__file__)
 file_dir = os.path.join(dir, 'regression_vault.grmpy.json')
 
-if True:
+if False:
     tests = []
     for seed in seeds:
         np.random.seed(seed)
@@ -35,16 +37,18 @@ if True:
     json.dump(tests, open(file_dir, 'w'))
 
 if True:
-    tests = json.load(open(file_dir, 'r'))
+
+    fname = PACKAGE_DIR + '/test/resources/regression_vault.grmpy.json'
+    tests = json.load(open(fname, 'r'))
 
     for test in tests:
         stat, dict_ = test
         print_dict(dict_)
         df = simulate('test.grmpy.ini')
         np.testing.assert_almost_equal(np.sum(df.sum()), stat)
-
-for f in glob.glob("*.grmpy.*"):
-    if f.startswith('regression'):
-        pass
-    else:
-        os.remove(f)
+#
+# for f in glob.glob("*.grmpy.*"):
+#     if f.startswith('regression'):
+#         pass
+#     else:
+#         os.remove(f)
