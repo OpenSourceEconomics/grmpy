e7b22a013fd207f26054984a6544347c809a820e
Sebastian Becker <s3sebeck@uni-bonn.de>
2019-03-31 22:24:55 +0200

    Some additional preparations in setup.py

--- setup.py
+++ setup.py
@@ -12,14 +12,24 @@ from shutil import rmtree
 from setuptools import find_packages, setup, Command
 
 # Package meta-data.
-NAME = 'grmpy'
-DESCRIPTION = 'grmpy is a package for simulation and analysis of the  generalized Roy model.'
-URL = 'http://grmpy.readthedocs.io'
-EMAIL = 'eisenhauer@policy-lab.org'
-AUTHOR = 'Philipp Eisenhauer'
+NAME = "grmpy"
+DESCRIPTION = "grmpy is a package for simulation and analysis of the  generalized Roy model."
+URL = "http://grmpy.readthedocs.io"
+EMAIL = "eisenhauer@policy-lab.org"
+AUTHOR = "Philipp Eisenhauer"
 
 # What packages are required for this module to be executed?
-REQUIRED = ['numpy', 'scipy', 'pytest', 'pandas', 'statsmodels']
+REQUIRED = [
+    "numpy",
+    "scipy",
+    "pytest",
+    "pandas",
+    "statsmodels",
+    "linearmodels",
+    "oyaml",
+    "matplotlib",
+    "seaborn",
+]
 
 # The rest you shouldn't have to touch too much :)
 # ------------------------------------------------
@@ -34,20 +44,20 @@ here = os.path.abspath(os.path.dirname(__file__))
 
 # Load the package's __version__.py module as a dictionary.
 about = {}
-with open(os.path.join(here, NAME, '__version__.py')) as f:
+with open(os.path.join(here, NAME, "__version__.py")) as f:
     exec(f.read(), about)
 
 
 class PublishCommand(Command):
     """Support setup.py publish."""
 
-    description = 'Build and publish the package.'
+    description = "Build and publish the package."
     user_options = []
 
     @staticmethod
     def status(s):
         """Prints things in bold."""
-        print('\033[1m{0}\033[0m'.format(s))
+        print("\033[1m{0}\033[0m".format(s))
 
     def initialize_options(self):
         pass
@@ -57,32 +67,30 @@ class PublishCommand(Command):
 
     def run(self):
         try:
-            self.status('Removing previous builds…')
-            rmtree(os.path.join(here, 'dist'))
+            self.status("Removing previous builds…")
+            rmtree(os.path.join(here, "dist"))
         except FileNotFoundError:
             pass
 
-        self.status('Building Source and Wheel (universal) distribution…')
-        os.system('{0} setup.py sdist bdist_wheel --universal'.format(sys.executable))
+        self.status("Building Source and Wheel (universal) distribution…")
+        os.system("{0} setup.py sdist bdist_wheel --universal".format(sys.executable))
 
-        self.status('Uploading the package to PyPi via Twine…')
-        os.system('twine upload dist/*')
+        self.status("Uploading the package to PyPi via Twine…")
+        os.system("twine upload dist/*")
 
         sys.exit()
 
 
 setup(
     name=NAME,
-    version=about['__version__'],
+    version=about["__version__"],
     description=DESCRIPTION,
     author=AUTHOR,
     author_email=EMAIL,
     url=URL,
-    packages=find_packages(exclude=('tests',)),
+    packages=find_packages(exclude=("tests",)),
     install_requires=REQUIRED,
-    license='MIT',
+    license="MIT",
     include_package_data=True,
-    cmdclass={
-        'publish': PublishCommand,
-    },
+    cmdclass={"publish": PublishCommand},
 )
