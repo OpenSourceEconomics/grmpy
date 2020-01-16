eee317f596a982d14c2722a0dec9adcc0a6e6ca4
Sebastian Gsell <sebastian.gsell93@gmail.com>
2020-01-16 02:58:31 +0100

    Trying let Travis run develop_test.ipynb.

--- utils/travis_runner.py
+++ utils/travis_runner.py
@@ -12,5 +12,14 @@ if __name__ == "__main__":
         "grmpy_tutorial_notebook.ipynb",
         "--ExecutePreprocessor.timeout=-1",
     ]
+    # subprocess.check_call(cmd)
+    # os.chdir("../..")
+
+    notebook = "develop_test.ipynb"
+    cmd = " jupyter nbconvert --execute {}  \
+        --ExecutePreprocessor.timeout=-1".format(
+        notebook
+    )
+    # sp.check_call(cmd, shell=True)
     subprocess.check_call(cmd)
     os.chdir("../..")
