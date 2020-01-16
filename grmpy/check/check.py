2d49050e416ba46ec708af5548004668e2837259
Sebastian Gsell <sebastian.gsell93@gmail.com>
2020-01-16 02:13:37 +0100

    Adding grmpy-semipar.

--- grmpy/check/check.py
+++ grmpy/check/check.py
@@ -23,23 +23,13 @@ def check_presence_estimation_dataset(init_dict):
         raise UserError(msg)
 
 
+
 def check_initialization_dict(dict_):
     """This function performs some basic checks regarding the integrity of the user's
     request. There should be no uncontrolled terminations of the package once these
     checks are passed.
     """
-    # Distribute details
-    num_agents_sim = dict_["SIMULATION"]["agents"]
-
-    # This are just two example for a whole host of tests.
-    if num_agents_sim <= 0:
-        msg = "The number of simulated individuals needs to be larger than zero."
-        raise UserError(msg)
-
-    if dict_["DETERMINISTIC"] is False:
-        if not is_pos_def(dict_):
-            msg = "The specified covariance matrix has to be positive semidefinite."
-            raise UserError(msg)
+    # Some basic checks
     for key_ in ["TREATED", "UNTREATED", "CHOICE"]:
         if len(dict_[key_]["order"]) > len(set(dict_[key_]["order"])):
             msg = (
@@ -49,9 +39,6 @@ def check_initialization_dict(dict_):
                 "same section.".format(key_)
             )
             raise UserError(msg)
-    error, msg = check_special_conf(dict_)
-    if error is True:
-        raise UserError(msg)
 
     if dict_["ESTIMATION"]["file"][-4:] not in [".pkl", ".txt", "dta"]:
         msg = (
@@ -61,6 +48,53 @@ def check_initialization_dict(dict_):
                 dict_["ESTIMATION"]["file"][-4:]
             )
         )
+        raise UserError(msg)
+
+
+def check_par(dict_):
+    """"""
+    # Distribute details
+    num_agents_sim = dict_["SIMULATION"]["agents"]
+
+    # These are examples for a whole host of tests.
+    if num_agents_sim <= 0:
+        msg = "The number of simulated individuals needs to be larger than zero."
+        raise UserError(msg)
+
+    if dict_["DETERMINISTIC"] is False:
+        if not is_pos_def(dict_):
+            msg = "The specified covariance matrix has to be positive semidefinite."
+            raise UserError(msg)
+
+    if all(dist_elements == 0 for dist_elements in dict_["DIST"]["params"]):
+        msg = "The distributional characteristics have to be undeterministic."
+        raise UserError(msg)
+    elif dict_["DIST"]["params"][5] == 0:
+        msg = (
+            "The standard deviation of the collected unobservables have to be larger"
+            " than zero."
+        )
+        raise UserError(msg)
+
+    for key_ in ["TREATED", "UNTREATED", "CHOICE"]:
+        if len(set(dict_[key_]["order"])) != len(dict_[key_]["order"]):
+            msg = "There are two start coefficients {} Section".format(key_)
+            raise UserError(msg)
+        if (
+                "params" not in dict_[key_].keys()
+                and dict_["ESTIMATION"]["start"] == "init"
+        ):
+            msg = (
+                "The missing of a pre-specified paramterization in the {} section does"
+                " not correspond with the start value option of your initialization "
+                "file. \n        We recommend to switch to the generation of automatic"
+                " start values by changing the start flag in the ESTIMATION section "
+                'from "init" to "auto".'.format(key_)
+            )
+            raise UserError(msg)
+
+    error, msg = check_special_conf(dict_)
+    if error is True:
         raise UserError(msg)
 
 
--- grmpy/estimate/estimate.py
+++ grmpy/estimate/estimate.py
@@ -1,81 +1,49 @@
 """The module provides an estimation process given the simulated data set and the
 initialization file.
 """
-from scipy.optimize import minimize
 import numpy as np
 
-from grmpy.estimate.estimate_auxiliary import backward_transformation
-from grmpy.estimate.estimate_auxiliary import minimizing_interface
-from grmpy.estimate.estimate_auxiliary import calculate_criteria
-from grmpy.estimate.estimate_auxiliary import optimizer_options
 from grmpy.check.check import check_presence_estimation_dataset
-from grmpy.estimate.estimate_output import write_comparison
-from grmpy.estimate.estimate_auxiliary import adjust_output
-from grmpy.estimate.estimate_auxiliary import start_values
-from grmpy.estimate.estimate_auxiliary import process_data
-from grmpy.estimate.estimate_output import print_logfile
-from grmpy.estimate.estimate_auxiliary import bfgs_dict
 from grmpy.check.check import check_initialization_dict
 from grmpy.check.check import check_presence_init
-from grmpy.check.check import check_init_file
-from grmpy.check.auxiliary import read_data
+from grmpy.check.check import check_par
 from grmpy.read.read import read
 
+from grmpy.estimate.estimate_semipar import semipar_fit
+from grmpy.estimate.estimate_par import par_fit
 
-def fit(init_file):
-    """The function estimates the coefficients of the simulated data set."""
+
+def fit(init_file, semipar=False):
+    """ """
     check_presence_init(init_file)
 
     dict_ = read(init_file)
-    np.random.seed(dict_["SIMULATION"]["seed"])
 
-    # We perform some basic consistency checks regarding the user's request.
+    # Perform some consistency checks given the user's request
     check_presence_estimation_dataset(dict_)
     check_initialization_dict(dict_)
-    check_init_file(dict_)
 
-    # Distribute initialization information.
-    data = read_data(dict_["ESTIMATION"]["file"])
-    num_treated = dict_["AUX"]["num_covars_treated"]
-    num_untreated = num_treated + dict_["AUX"]["num_covars_untreated"]
+    # Semiparametric Model
+    if semipar is True:
+        quantiles, mte_u, X, b1_b0 = semipar_fit(init_file) # change to dict_
 
-    _, X1, X0, Z1, Z0, Y1, Y0 = process_data(data, dict_)
+        # Construct MTE
+        # Calculate the MTE component that depends on X
+        mte_x = np.dot(X, b1_b0)
 
-    if dict_["ESTIMATION"]["maxiter"] == 0:
-        option = "init"
+        # Put the MTE together
+        mte = mte_x.mean(axis=0) + mte_u
+
+        # Accounting for variation in X
+        mte_min = np.min(mte_x) + mte_u
+        mte_max = np.max(mte_x) + mte_u
+
+        rslt = {"quantiles": quantiles, "mte": mte, "mte_x": mte_x, "mte_u": mte_u,
+                "mte_min": mte_min, "mte_max": mte_max, "X": X, "b1-b0": b1_b0}
+
+    # Parametric Normal Model
     else:
-        option = dict_["ESTIMATION"]["start"]
-
-    # Read data frame
-
-    # define starting values
-    x0 = start_values(dict_, data, option)
-    opts, method = optimizer_options(dict_)
-    dict_["AUX"]["criteria"] = calculate_criteria(dict_, X1, X0, Z1, Z0, Y1, Y0, x0)
-    dict_["AUX"]["starting_values"] = backward_transformation(x0)
-    rslt_dict = bfgs_dict()
-    if opts["maxiter"] == 0:
-        rslt = adjust_output(None, dict_, x0, X1, X0, Z1, Z0, Y1, Y0, rslt_dict)
-    else:
-        opt_rslt = minimize(
-            minimizing_interface,
-            x0,
-            args=(dict_, X1, X0, Z1, Z0, Y1, Y0, num_treated, num_untreated, rslt_dict),
-            method=method,
-            options=opts,
-        )
-        rslt = adjust_output(
-            opt_rslt, dict_, opt_rslt["x"], X1, X0, Z1, Z0, Y1, Y0, rslt_dict
-        )
-    # Print Output files
-    print_logfile(dict_, rslt)
-
-    if "comparison" in dict_["ESTIMATION"].keys():
-        if dict_["ESTIMATION"]["comparison"] == 0:
-            pass
-        else:
-            write_comparison(data, rslt)
-    else:
-        write_comparison(data, rslt)
+        check_par(dict_)
+        rslt = par_fit(dict_)
 
     return rslt
