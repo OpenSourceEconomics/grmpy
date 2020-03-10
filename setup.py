502baecc85f5580801636b8ff38c9fcc0f5baa71
Sebastian Gsell <sebastian.gsell93@gmail.com>
2020-03-10 18:04:44 +0100

    Updated setup.py and development version number.

--- setup.py
+++ setup.py
@@ -4,19 +4,20 @@
 # Note: To use the 'upload' functionality of this file, you must:
 #   $ pip install twine
 
-import io
 import os
 import sys
+from pathlib import Path
 from shutil import rmtree
 
 from setuptools import find_packages, setup, Command
 
 # Package meta-data.
 NAME = "grmpy"
-DESCRIPTION = "grmpy is a package for the simulation and analysis of the generalized Roy model."
+DESCRIPTION = "grmpy is a Python package for the simulation and estimation of the generalized Roy model."
+README = Path("README.md").read_text()
 URL = "http://grmpy.readthedocs.io"
 EMAIL = "eisenhauer@policy-lab.org"
-AUTHOR = "Philipp Eisenhauer"
+AUTHOR = "The grmpy Development Team"
 
 # What packages are required for this module to be executed?
 REQUIRED = [
@@ -30,7 +31,7 @@ REQUIRED = [
     "matplotlib",
     "seaborn",
     "scikit-misc",
-    "numba"
+    "numba",
 ]
 
 # The rest you shouldn't have to touch too much :)
@@ -87,6 +88,8 @@ setup(
     name=NAME,
     version=about["__version__"],
     description=DESCRIPTION,
+    long_description=DESCRIPTION + "\n\n" + README,
+    long_description_content_type="text/markdown",
     author=AUTHOR,
     author_email=EMAIL,
     url=URL,
