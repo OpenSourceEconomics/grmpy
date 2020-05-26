aef64529bec97e6cc54e08eea6775b3d54294cec
Sebastian Gsell <sebastian.gsell93@gmail.com>
2020-05-26 21:27:01 +0200

    Investiagting travis timeout error.

--- utils/travis_runner.py
+++ utils/travis_runner.py
@@ -16,13 +16,13 @@ if __name__ == "__main__":
     os.chdir("../..")
 
 
-if __name__ == "__main__":
-    os.chdir("promotion/grmpy_tutorial_notebook")
-    cmd = [
-        "jupyter",
-        "nbconvert",
-        "--execute",
-        "tutorial_semipar_notebook.ipynb",
-        "--ExecutePreprocessor.timeout=-1",
-    ]
-    subprocess.check_call(cmd)
+# if __name__ == "__main__":
+#     os.chdir("promotion/grmpy_tutorial_notebook")
+#     cmd = [
+#         "jupyter",
+#         "nbconvert",
+#         "--execute",
+#         "tutorial_semipar_notebook.ipynb",
+#         "--ExecutePreprocessor.timeout=-1",
+#     ]
+#     subprocess.check_call(cmd)
