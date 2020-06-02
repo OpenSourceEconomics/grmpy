Adjusted the computation process of the hessian matrix, solved bug in the estimation output function

grmpy/estimate/estimate_par.py
@@ -6,7 +6,7 @@ import statsmodels.api as sm
 from numpy.linalg import LinAlgError
 from scipy.optimize import minimize
 from scipy.stats import norm, t
-from statsmodels.tools.numdiff import approx_hess_cs
+from statsmodels.tools.numdiff import approx_fprime_cs
 from statsmodels.tools.sm_exceptions import PerfectSeparationError
 
 from grmpy.check.auxiliary import read_data
@@ -34,7 +34,7 @@ def par_fit(dict_):
 
     # define starting values
     x0 = start_values(dict_, data, option)
-    opts, method = optimizer_options(dict_)
+    opts, method, grad_opt = optimizer_options(dict_)
     dict_["AUX"]["criteria"] = calculate_criteria(dict_, X1, X0, Z1, Z0, Y1, Y0, x0)
     dict_["AUX"]["starting_values"] = backward_transformation(x0)
     rslt_dict = bfgs_dict()
@@ -44,10 +44,21 @@ def par_fit(dict_):
         opt_rslt = minimize(
             minimizing_interface,
             x0,
-            args=(dict_, X1, X0, Z1, Z0, Y1, Y0, num_treated, num_untreated, rslt_dict),
+            args=(
+                X1,
+                X0,
+                Z1,
+                Z0,
+                Y1,
+                Y0,
+                num_treated,
+                num_untreated,
+                rslt_dict,
+                grad_opt,
+            ),
             method=method,
             options=opts,
-            jac=True,
+            jac=grad_opt,
         )
         rslt = adjust_output(
             opt_rslt, dict_, opt_rslt["x"], X1, X0, Z1, Z0, Y1, Y0, rslt_dict
@@ -101,14 +112,7 @@ def start_values(init_dict, data_frame, option):
         # Set coefficients equal the true init file values
         x0 = init_dict["AUX"]["init_values"][:-6]
 
-    if option == "random":
-        num_cov = (
-            len(init_dict["TREATED"]["order"])
-            + len(init_dict["UNTREATED"]["order"])
-            + len(init_dict["CHOICE"]["order"])
-        )
-        x0 = np.random.normal(0, 1, size=num_cov)
-    elif option == "auto":
+    elif option in ["auto", "random"]:
 
         try:
 
@@ -123,14 +127,13 @@ def start_values(init_dict, data_frame, option):
                 else:
                     order = init_dict["UNTREATED"]["order"]
                 X = data_frame[order][i == data_frame[indicator]]
-
                 ols_results = sm.OLS(Y, X).fit()
                 beta += [ols_results.params]
-                # TODO: how to handle start values for rho, setting rho=0.0 leads to crazy values for the replication
-                rho = np.random.uniform(
-                    -np.sqrt(ols_results.scale), np.sqrt(ols_results.scale), 1
-                ) / np.sqrt(ols_results.scale)
-                sd_ += [np.sqrt(ols_results.scale), rho[0]]
+                if option == "auto":
+                    rho = np.random.uniform(
+                        -np.sqrt(ols_results.scale), np.sqrt(ols_results.scale), 1
+                    ) / np.sqrt(ols_results.scale)
+                    sd_ += [np.sqrt(ols_results.scale), rho[0]]
             # Estimate gamma via Probit
             Z = data_frame[init_dict["CHOICE"]["order"]]
             probitRslt = sm.Probit(data_frame[indicator], Z).fit(disp=0)
@@ -158,8 +161,8 @@ def start_values(init_dict, data_frame, option):
     return x0
 
 
-def start_value_adjustment(x, init_dict, option, seed=None):
-    """This function transforms the rho values so that they are always between zero and 1.
+def start_value_adjustment(x, init_dict, option):
+    """This function transforms the rho values so that they are always between zero and 1
     Additionally it transforms the sigma values so that all values are always positive.
     """
 
@@ -177,7 +180,7 @@ def start_value_adjustment(x, init_dict, option, seed=None):
         x = np.concatenate((x, dist))
 
     elif option == "random":
-        sds = np.random.uniform(0, 1, 2)
+        sds = np.random.uniform(0, 0.5, 2)
         rho1 = np.random.uniform(-sds[0], sds[0], 1) / sds[0]
         rho0 = np.random.uniform(-sds[1], sds[1], 1) / sds[1]
         x = np.concatenate((x, [sds[0], rho1[0], sds[1], rho0[0]]))
@@ -211,7 +214,9 @@ def backward_transformation(x0, dict_=None):
     return x
 
 
-def log_likelihood(x0, X1, X0, Z1, Z0, Y1, Y0, num_treated, num_untreated, dict_=None):
+def log_likelihood(
+    x0, X1, X0, Z1, Z0, Y1, Y0, num_treated, num_untreated, dict_=None, grad_opt=True
+):
     """The function provides the log-likelihood function for the minimization process."""
 
     beta1, beta0, gamma = (
@@ -232,40 +237,18 @@ def log_likelihood(x0, X1, X0, Z1, Z0, Y1, Y0, num_treated, num_untreated, dict_
     untreated = (1 / sd0) * norm.pdf(nu0) * (1 - norm.cdf(lambda0))
 
     likl = -np.mean(np.log(np.append(treated, untreated)))
-    llh_grad = gradient(
-        X1, X0, Z1, Z0, nu1, nu0, lambda1, lambda0, gamma, sd1, sd0, rho1v, rho0v
-    )
     if dict_ is None:
         pass
     else:
         dict_["crit"][str(len(dict_["crit"]))] = likl
-        dict_["grad"][str(len(dict_["crit"]))] = llh_grad
 
-    return likl, llh_grad
-
-
-def log_likelihood_hess(x0, X1, X0, Z1, Z0, Y1, Y0, num_treated, num_untreated):
-    """The function provides the log-likelihood function for the minimization process."""
-
-    beta1, beta0, gamma = (
-        x0[:num_treated],
-        x0[num_treated:num_untreated],
-        x0[num_untreated:-4],
-    )
-    sd1, sd0, rho1v, rho0v = x0[-4], x0[-2], x0[-3], x0[-1]
-    # Provide parameterization for D=1 and D=0 and provide auxiliary list likl
-
-    nu1 = (Y1 - np.dot(beta1, X1.T)) / sd1
-    lambda1 = (np.dot(gamma, Z1.T) - rho1v * nu1) / (np.sqrt((1 - rho1v ** 2)))
-
-    nu0 = (Y0 - np.dot(beta0, X0.T)) / sd0
-    lambda0 = (np.dot(gamma, Z0.T) - rho0v * nu0) / (np.sqrt((1 - rho0v ** 2)))
-
-    treated = (1 / sd1) * norm.pdf(nu1) * norm.cdf(lambda1)
-    untreated = (1 / sd0) * norm.pdf(nu0) * (1 - norm.cdf(lambda0))
-
-    likl = -np.mean(np.log(np.clip(np.append(treated, untreated), 1e-20, np.inf)))
-    return likl
+    if grad_opt is True:
+        llh_grad = gradient(
+            X1, X0, Z1, Z0, nu1, nu0, lambda1, lambda0, gamma, sd1, sd0, rho1v, rho0v
+        )
+        return likl, llh_grad
+    else:
+        return likl
 
 
 def calculate_criteria(init_dict, X1, X0, Z1, Z0, Y1, Y0, x0):
@@ -273,7 +256,9 @@ def calculate_criteria(init_dict, X1, X0, Z1, Z0, Y1, Y0, x0):
     x = backward_transformation(x0)
     num_treated = init_dict["AUX"]["num_covars_treated"]
     num_untreated = num_treated + init_dict["AUX"]["num_covars_untreated"]
-    criteria, _ = log_likelihood(x, X1, X0, Z1, Z0, Y1, Y0, num_treated, num_untreated)
+    criteria = log_likelihood(
+        x, X1, X0, Z1, Z0, Y1, Y0, num_treated, num_untreated, None, False
+    )
     return criteria
 
 
@@ -284,21 +269,21 @@ def optimizer_options(init_dict_):
     method = init_dict_["ESTIMATION"]["optimizer"]
     opt_dict = init_dict_["SCIPY-" + method]
     opt_dict["maxiter"] = init_dict_["ESTIMATION"]["maxiter"]
-    return opt_dict, method
+    grad_opt = method == "BFGS"
+    return opt_dict, method, grad_opt
 
 
 def minimizing_interface(
-    x0, init_dict, X1, X0, Z1, Z0, Y1, Y0, num_treated, num_untreated, dict_
+    x0, X1, X0, Z1, Z0, Y1, Y0, num_treated, num_untreated, dict_, grad_opt
 ):
     """The function provides the minimization interface for the estimation process."""
     # Collect arguments
     x0 = backward_transformation(x0, dict_)
     # Calculate likelihood for pre-specified arguments
-    likl, llh_grad = log_likelihood(
-        x0, X1, X0, Z1, Z0, Y1, Y0, num_treated, num_untreated, dict_
-    )
 
-    return likl, llh_grad
+    return log_likelihood(
+        x0, X1, X0, Z1, Z0, Y1, Y0, num_treated, num_untreated, dict_, grad_opt
+    )
 
 
 def adjust_output(opt_rslt, init_dict, x0, X1, X0, Z1, Z0, Y1, Y0, dict_=None):
@@ -476,8 +461,8 @@ def process_output(init_dict, dict_, x0, flag):
 
 
 def bfgs_dict():
-    """The function provides a dictionary for tracking the criteria function values and the
-    associated parametrization.
+    """The function provides a dictionary for tracking the criteria function values and
+    the associated parametrization.
     """
     rslt_dict = {"parameter": {}, "crit": {}, "grad": {}}
 
@@ -500,11 +485,7 @@ def calculate_se(x, init_dict, X1, X0, Z1, Z0, Y1, Y0, num_treated, num_untreate
     else:
         norm_value = norm.ppf(0.975)
         # Calculate the hessian matrix, check if it is p
-        hess = approx_hess_cs(
-            x0,
-            log_likelihood_hess,
-            args=(X1, X0, Z1, Z0, Y1, Y0, num_treated, num_untreated),
-        )
+        hess = approx_fprime_cs(x0, gradient_hessian, args=(X1, X0, Z1, Z0, Y1, Y0))
         try:
             hess_inv = np.linalg.inv(hess)
             se = np.sqrt(np.diag(hess_inv) / num_ind)
@@ -590,6 +571,87 @@ def gradient(X1, X0, Z1, Z0, nu1, nu0, lambda1, lambda0, gamma, sd1, sd0, rho1v,
         / (1 - rho0v ** 2) ** (1 / 2)
     )
 
+    grad_gamma = sum(
+        np.einsum(
+            "ij, i ->ij",
+            Z1,
+            (norm.pdf(lambda1) / norm.cdf(lambda1)) * 1 / np.sqrt(1 - rho1v ** 2),
+        )
+    ) - sum(
+        np.einsum(
+            "ij, i ->ij",
+            Z0,
+            (norm.pdf(lambda0) / (1 - norm.cdf(lambda0)))
+            * (1 / np.sqrt(1 - rho0v ** 2)),
+        )
+    )
+
+    grad = np.sum(np.einsum("ij, i ->ij", X1, -grad_beta1), 0)
+    grad = np.append(grad, np.sum(np.einsum("ij, i ->ij", X0, -grad_beta0), 0))
+    grad = np.append(grad, -grad_gamma)
+    grad = np.append(
+        grad,
+        np.append(np.sum([grad_sd1, grad_rho1v], 1), np.sum([grad_sd0, grad_rho0v], 1)),
+    )
+
+    return grad / n
+
+
+def gradient_hessian(x0, X1, X0, Z1, Z0, Y1, Y0):
+
+    num_treated = X1.shape[1]
+    num_untreated = num_treated + X0.shape[1]
+
+    beta1, beta0, gamma = (
+        x0[:num_treated],
+        x0[num_treated:num_untreated],
+        x0[num_untreated:-4],
+    )
+    sd1, sd0, rho1v, rho0v = x0[-4], x0[-2], x0[-3], x0[-1]
+
+    n_obs = Y1.shape[0] + Y0.shape[0]
+    # compute gradient for beta 1
+
+    nu1 = (Y1 - np.dot(beta1, X1.T)) / sd1
+    lambda1 = (np.dot(gamma, Z1.T) - rho1v * nu1) / (np.sqrt((1 - rho1v ** 2)))
+
+    nu0 = (Y0 - np.dot(beta0, X0.T)) / sd0
+    lambda0 = (np.dot(gamma, Z0.T) - rho0v * nu0) / (np.sqrt((1 - rho0v ** 2)))
+
+    grad_beta1 = (norm.pdf(lambda1) / norm.cdf(lambda1)) * (
+        rho1v / (np.sqrt(1 - rho1v ** 2) * sd1)
+    ) + nu1 / sd1
+
+    grad_beta0 = (
+        -norm.pdf(lambda0)
+        / (1 - norm.cdf(lambda0))
+        * (rho0v / (np.sqrt(1 - rho0v ** 2) * sd0))
+        + nu0 / sd0
+    )
+
+    grad_sd1 = sd1 * (
+        +1 / sd1
+        - (norm.pdf(lambda1) / norm.cdf(lambda1))
+        * (rho1v * nu1 / (np.sqrt(1 - rho1v ** 2) * sd1))
+        - nu1 ** 2 / sd1
+    )
+    grad_sd0 = sd0 * (
+        +1 / sd0
+        + (norm.pdf(lambda0) / (1 - norm.cdf(lambda0)))
+        * (rho0v * nu0 / (np.sqrt(1 - rho0v ** 2) * sd0))
+        - nu0 ** 2 / sd0
+    )
+    grad_rho1v = (
+        -(norm.pdf(lambda1) / norm.cdf(lambda1))
+        * ((np.dot(gamma, Z1.T) * rho1v) - nu1)
+        / (1 - rho1v ** 2) ** (3 / 2)
+    )
+    grad_rho0v = (
+        (norm.pdf(lambda0) / (1 - norm.cdf(lambda0)))
+        * ((np.dot(gamma, Z0.T) * rho0v) - nu0)
+        / (1 - rho0v ** 2) ** (3 / 2)
+    )
+
     grad_gamma = np.sum(
         np.einsum(
             "ij, i ->ij",
@@ -600,17 +662,18 @@ def gradient(X1, X0, Z1, Z0, nu1, nu0, lambda1, lambda0, gamma, sd1, sd0, rho1v,
     ) - sum(
         np.einsum(
             "ij, i ->ij",
-            Z0,
+            Z0[:, :],
             (norm.pdf(lambda0) / (1 - norm.cdf(lambda0)))
             * (1 / np.sqrt(1 - rho0v ** 2)),
         )
     )
 
-    grad = np.sum(np.einsum("ij, i ->ij", X1, -grad_beta1), 0) / n
-    grad = np.append(grad, np.sum(np.einsum("ij, i ->ij", X0, -grad_beta0), 0) / n)
-    grad = np.append(grad, -grad_gamma / n)
-    grad = np.append(grad, sum(grad_sd1) / n)
-    grad = np.append(grad, sum(grad_rho1v) / n)
-    grad = np.append(grad, sum(grad_sd0) / n)
-    grad = np.append(grad, sum(grad_rho0v) / n)
-    return grad
+    grad = np.sum(np.einsum("ij, i ->ij", X1, -grad_beta1), 0)
+    grad = np.append(grad, np.sum(np.einsum("ij, i ->ij", X0, -grad_beta0), 0))
+    grad = np.append(grad, -grad_gamma)
+    grad = np.append(
+        grad,
+        np.append(np.sum([grad_sd1, grad_rho1v], 1), np.sum([grad_sd0, grad_rho0v], 1)),
+    )
+
+    return grad / n_obs